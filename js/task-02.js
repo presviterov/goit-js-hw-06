9b493c261ec46726e49683639d8a39ab1475652e
Presviterov <presviterov@gmail.com>
2022-05-16 17:38:38 +0300

    homework-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const foodList = document.getElementById("ingredients");
+const foodIngredient = ingredients.forEach(ingredient => {
+  let item = document.createElement("li");
+  item.className = "item";
+  item.innerHTML = ingredient;
+  foodList.append(item);
+  });
+
+
+// const markup = ingredients
+//  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//  .join("");
