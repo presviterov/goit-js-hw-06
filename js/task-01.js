179edd6d620a8c885abedc5b89ec948988add589
Presviterov <presviterov@gmail.com>
2022-05-16 16:05:25 +0300

    first homework

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,7 @@
-TEST
+const countLi = document.querySelectorAll('li.item');
+console.log('Number of categories:', countLi.length);
+
+countLi.forEach(function (item, index) {
+    console.log('Category:', item.firstElementChild.outerText);
+    console.log('Elements:', item.lastElementChild.children.length);
+  });
